c6499d548514e49939252213d35e6353f396876c
Joel Markgren <joeel.markgren@gmail.com>
2024-04-11 15:13:07 +0200

    Categories and BrokerageFirm Controllers added

--- HomeHavenAPI/Data/Repos/BrokerageFirmRepository.cs
+++ HomeHavenAPI/Data/Repos/BrokerageFirmRepository.cs
@@ -1,8 +1,26 @@
 ﻿using HomeHavenAPI.Data.Interfaces;
+using HomeHavenAPI.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HomeHavenAPI.Data.Repos
 {
     public class BrokerageFirmRepository : IBrokerageFirm
     {
+        private readonly ApplicationDbContext applicationDbContext;
+
+        public BrokerageFirmRepository(ApplicationDbContext applicationDbContext)
+        {
+            this.applicationDbContext = applicationDbContext;
+        }
+        public async Task<BrokerageFirm> Get(int id)
+        {
+            return await applicationDbContext.Firms.FindAsync(id);
+        }
+
+        public async Task<IEnumerable<BrokerageFirm>> GetAll()
+        {
+            var firms = await applicationDbContext.Firms.ToListAsync();
+            return firms;
+        }
     }
 }
