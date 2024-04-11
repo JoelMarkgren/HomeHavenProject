0021e5e758cfd82f283b391bef58943b4c767317
Joel Markgren <joeel.markgren@gmail.com>
2024-04-11 14:16:08 +0200

    Region Controller Added

---
+++ HomeHavenAPI/Controllers/RegionController.cs
@@ -0,0 +1,49 @@
+﻿using HomeHavenAPI.Data.Interfaces;
+using HomeHavenAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace HomeHavenAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RegionController : ControllerBase
+    {
+        private readonly IRegion regionRepo;
+
+        public RegionController(IRegion regionRepo)
+        {
+            this.regionRepo = regionRepo;
+        }
+        // GET: api/<RegionController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Region>>> GetAll()
+        {
+            var regions = await regionRepo.GetAll();
+            if (regions == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(regions);
+            }
+        }
+
+        // GET api/<RegionController>/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Region>> Get(int id)
+        {
+            Region region = await regionRepo.Get(id);
+            if (region == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(region);
+            }
+        }
+    }
+}
--- HomeHavenAPI/Data/Interfaces/IRegion.cs
+++ HomeHavenAPI/Data/Interfaces/IRegion.cs
@@ -1,6 +1,11 @@
-﻿namespace HomeHavenAPI.Data.Interfaces
+﻿using HomeHavenAPI.Models;
+
+namespace HomeHavenAPI.Data.Interfaces
 {
     public interface IRegion
     {
+        Task<Region> Get(int id);
+        Task<IEnumerable<Region>> GetAll();
+        
     }
 }
